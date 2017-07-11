a0937007c878603e3266496b3c99f7da2bf964e6
tatsuki.nakagawa <tatsukinakagawa14@gmail.com>
2017-07-12 01:02:03 +0900

    アップロードできるファイルサイズの更新

--- docker/image_resize_app/launch.sh
+++ docker/image_resize_app/launch.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-/usr/sbin/php-fpm5.6 -c /etc/php/5.6/fpm/php-fpm.conf -F
+#/usr/sbin/php-fpm5.6 -c /etc/php/5.6/fpm/php-fpm.conf -F
+service php5.6-fpm start
+
+trap 'service php5.6-fpm stop; exit 0' EXIT
+
+tail -f /dev/null
